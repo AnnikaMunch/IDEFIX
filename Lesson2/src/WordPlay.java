b87858c0bdf3d12542319b65fa479a36ba226f9c
AnnikaMunch <annie_munch@hotmail.com>
2013-02-15 11:25:34 +0100

    Slette kommentar

--- Lesson2/src/WordPlay.java
+++ Lesson2/src/WordPlay.java
@@ -45,28 +45,5 @@ public class WordPlay {
 			System.out.println(testString + " is NOT a Palindrome!");
 		}
 	}
-		/*
-		int i, begin, end, middle;
 		
-		begin = 0;
-		end = length - 1;
-		middle = (begin + end)/2;
-		
-		for (i = begin; i<= middle; i++) {
-			if (palindrome.charAt(begin) == palindrome.charAt(end)) {
-				begin++;
-				end--;
-			}
-			else {
-				break;
-			}
-		}
-		if (i == middle + 1) {
-			System.out.println("Palindrome");
-		}
-		else {
-			System.out.println("Not a palindrome");
-		}
-	}*/
-
 }
